dc361b306f931f6cb7d55deeb8e659c16c5c84df
Codrea123 <olahcodrut@gmail.com>
2023-01-02 22:07:35 +0200

    QOL changes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -85,6 +85,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{route('income.index')}}" :active="request()->routeIs('income.index')">
+                {{ __('All income') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{route('categories.index')}}" :active="request()->routeIs('categories.index')">
+                {{ __('All categories') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
