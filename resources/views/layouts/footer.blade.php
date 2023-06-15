29fc2ca79038cef6caa339a1d2b2a208458e04d3
Codrut <olahcodrut@gmail.com>
2023-06-15 22:07:13 +0300

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="text-center">
-    {{ \Carbon\Carbon::now()->year }} &copy; Olah David Codrut. All Rights Reserved.
+    {{ \Carbon\Carbon::now()->year }} &copy; Jula Mihai. All Rights Reserved.
 </footer>
